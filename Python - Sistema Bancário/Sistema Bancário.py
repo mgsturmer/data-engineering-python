6250f80ab53eaf6eca3e3d12a74f88579cf360de
mgsturmer <mgsturmer@gmail.com>
2025-01-03 10:52:36 -0300

    Update Sistema Bancário.py
    Corrigindo e melhroando o código

--- Python - Sistema Bancário/Sistema Bancário.py
+++ Python - Sistema Bancário/Sistema Bancário.py
@@ -1,57 +1,50 @@
 menu = """
 
-[d] Depositar
-[s] Sacar
-[e] Extrato
-[q] Sair
+[1] Depositar
+[2] Sacar
+[3] Extrato
+[0] Sair
 
 => """
 
-saldo = 0.0
-limite = 500.0
+saldo = 0.00
+limite = 500.00
 extrato = []
 numero_saques = 0
 LIMITE_SAQUES = 3
 
 while True:
-    opcao = input(menu).lower()
+    opcao = int(input(menu))
 
-    if opcao == "d":
-        try:
+    if opcao == 1:
             valor = float(input("Informe o valor do depósito: "))
             if valor > 0:
                 saldo += valor
+                ##Append do valor do saque no extrato para manter todos registrados. 
                 extrato.append(f"Depósito: R$ {valor:.2f}")
                 print(f"Depósito de R$ {valor:.2f} realizado com sucesso.")
             else:
                 print("Operação falhou! O valor informado é inválido.")
-        except ValueError:
-            print("Valor inválido. Tente novamente.")
 
-    elif opcao == "s":
-        try:
+    elif opcao == 2:
             valor = float(input("Informe o valor do saque: "))
-            excedeu_saldo = valor > saldo
-            excedeu_limite = valor > limite
-            excedeu_saques = numero_saques >= LIMITE_SAQUES
 
-            if excedeu_saldo:
+            if valor > saldo:
                 print("Operação falhou! Você não tem saldo suficiente.")
-            elif excedeu_limite:
+            elif valor > limite:
                 print("Operação falhou! O valor do saque excede o limite.")
-            elif excedeu_saques:
+            elif numero_saques >= LIMITE_SAQUES:
                 print("Operação falhou! Número máximo de saques excedido.")
             elif valor > 0:
                 saldo -= valor
+                ##Append do valor do saque no extrato para manter todos registrados.
                 extrato.append(f"Saque: R$ {valor:.2f}")
                 numero_saques += 1
                 print(f"Saque de R$ {valor:.2f} realizado com sucesso.")
             else:
                 print("Operação falhou! O valor informado é inválido.")
-        except ValueError:
-            print("Valor inválido. Tente novamente.")
 
-    elif opcao == "e":
+    elif opcao == 3:
         print("\n================ EXTRATO ================")
         if extrato:
             for linha in extrato:
@@ -61,7 +54,7 @@ while True:
         print(f"\nSaldo: R$ {saldo:.2f}")
         print("==========================================")
 
-    elif opcao == "q":
+    elif opcao == 0:
         print("Obrigado por utilizar nosso sistema bancário. Até logo!")
         break
 
